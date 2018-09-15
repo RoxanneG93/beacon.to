1e45db0f8042a7be0b90f12bc8dac92bc1dc60b8
Jeremy P <rolandojpena88@gmail.com>
2018-09-15 16:17:48 -0500

    added photo functionality on main page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,87 +3,30 @@ import { NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview';
 
-// camera options (Size and location). In the following example, the preview uses the rear camera and display the preview in the back of the webview
-const cameraPreviewOpts: CameraPreviewOptions = {
-  x: 0,
-  y: 0,
-  width: window.screen.width,
-  height: window.screen.height,
-  camera: 'rear',
-  tapPhoto: true,
-  previewDrag: true,
-  toBack: true,
-  alpha: 1
-};
-
-
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public base64Image: string; v
 
-  constructor(public navCtrl: NavController, private camera: Camera, private cameraPreview: CameraPreview) { }
+  constructor(public navCtrl: NavController, private camera: Camera) { }
 
-  cameraFunc() {
-    // start camera
-    this.cameraPreview.startCamera(cameraPreviewOpts).then(
-      (res) => {
-        console.log(res)
-      },
-      (err) => {
-        console.log(err)
-      });
+  takePicture() {
 
-    // Set the handler to run every time we take a picture
-    this.cameraPreview.setOnPictureTakenHandler().subscribe((result) => {
-      console.log(result);
-      // do something with the result
-    });
-
-
-    // picture options
-    const pictureOpts: CameraPreviewPictureOptions = {
-      width: 1280,
-      height: 1280,
-      quality: 85
-    }
-
-    // take a picture
-    this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
-      this.picture = 'data:image/jpeg;base64,' + imageData;
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+      //      targetWidth: 1000,
+      //      targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+      this.base64Image = "data:image/jpeg;base64," + imageData;
     }, (err) => {
       console.log(err);
-      this.picture = 'assets/img/test.jpg';
-    });
-
-    // Switch camera
-    this.cameraPreview.switchCamera();
-
-    // set color effect to negative
-    this.cameraPreview.setColorEffect('negative');
-
-    // Stop the camera preview
-    this.cameraPreview.stopCamera();,
+    }
+    );
   }
-
-
-
-
-
-  // const options: CameraOptions = {
-  //   quality: 100,
-  //   destinationType: 1, // this.camera.DestinationType.FILE_URI,
-  //   encodingType: 0, // this.camera.EncodingType.JPEG,
-  //   mediaType: 0 //this.camera.MediaType.PICTURE
-  // }
-
-  // this.camera.getPicture(options).then((imageData) => {
-  //   // imageData is either a base64 encoded string or a file URI
-  //   // If it's base64 (DATA_URL):
-  //   let base64Image = 'data:image/jpeg;base64,' + imageData;
-  // }, (err) => {
-  //   // Handle error
-  // });
 }
